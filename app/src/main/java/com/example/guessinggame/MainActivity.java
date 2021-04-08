fd8874098044f5ff1b9b54d1a027540c9c6ba3ca
Reno Facundo <renofacundo@outlook.com>
2021-04-08 10:42:33 +0900

    Added difficult settings and about alert dialogs under selection menu.

--- app/src/main/java/com/example/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/guessinggame/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -24,7 +25,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText txtGuess;
     private Button btnGuess;
     private TextView lblOutput;
+    private TextView lblRange;
     private int theNumber, numberOfTries;
+    private int range = 100;
     private boolean gameOver;
 
     public void checkGuess() {
@@ -49,12 +52,11 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
             } //Win block
         } catch(Exception e) {
-            message = "Please enter a whole number between 1 and 100.";
+            message = "Please enter a whole number between 1 and " + range + ":";
         } finally {
             if ((numberOfTries < 1) && (!gameOver)) {
                 gameOver = true;
-                message = "You lose!\n" +
-                        "The number was " + theNumber;
+                message = "You lose!\n" + "The number was " + theNumber;
                 btnGuess.setText("Play Again");
                 lblOutput.setText(message);
                 Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
@@ -69,11 +71,14 @@ public class MainActivity extends AppCompatActivity {
     public void newGame() {
         gameOver = false;
         lblOutput.setText("");
+        lblRange.setText("Enter a number between 1 and " + range + ":");
+        txtGuess.setText("");
         txtGuess.requestFocus();
-        theNumber = (int)(Math.random() * 100 + 1);
+        btnGuess.setText("Guess");
+        theNumber = (int)(Math.random() * range + 1);
         //theNumber = 99;
         numberOfTries = 7;
-        btnGuess.setText("Guess");
+
     }
 
     @Override
@@ -84,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
         txtGuess = (EditText) findViewById(R.id.txtGuess);
         btnGuess = (Button) findViewById(R.id.btnGuess);
         lblOutput = (TextView) findViewById(R.id.lblOutput);
+        lblRange = (TextView) findViewById(R.id.lblRange);
         newGame();
         btnGuess.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,10 +134,58 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                final CharSequence[] items = {"1 to 10", "1 to 100", "1 to 1000"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Select the range:");
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch(which) {
+                            case 0:
+                                range = 10;
+                                newGame();
+                                break;
+                            case 2:
+                                range = 1000;
+                                newGame();
+                                break;
+                            case 1:
+                            default:
+                                range = 100;
+                                newGame();
+                                break;
+                        }
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+                return true;
+            case R.id.action_newgame:
+                newGame();
+                return true;
+            case R.id.action_about:
+                AlertDialog aboutDialog = new AlertDialog.Builder(MainActivity.this).create();
+                aboutDialog.setTitle("About Guessing Game");
+                aboutDialog.setMessage("(c) 2021 Reno Facundo");
+                aboutDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                aboutDialog.show();
+                return true;
+            case R.id.action_game_stats:
+                return true;
+            default:
+                //return super.onOptionsItemSelected(item);
+                break;
         }
+
 
         return super.onOptionsItemSelected(item);
     }
